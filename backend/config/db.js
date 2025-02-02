ece8bbfd9b18af2f042618625d96284e8bd1737a
Ignacio Marzotta <ignaciomarzotta@gmail.com>
2025-02-02 18:46:07 -0300

    Minor changes to database setup and navbar frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,14 +5,11 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI, { });
     console.log('Conectado a MongoDB');
   } catch (error) {
     console.error('Error de conexión a MongoDB:', error.message);
-    process.exit(1); // Detener la aplicación si falla la conexión
+    process.exit(1);
   }
 };
 
